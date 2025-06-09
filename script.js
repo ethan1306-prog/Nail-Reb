6395f00210209141aa8a427d0acc7e03689b396d
ethan1306-prog <ethan.ascoli@gmail.com>
2025-06-09 19:31:53 +0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Simple module admin simulation
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Admin module JS loaded');
-    // Simule une vérification d'accès admin
-    const isAdmin = false;
-    if (isAdmin) {
-        alert("Bienvenue dans l'espace admin !");
-    }
-});
